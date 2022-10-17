468fd4bac774984e80799ded4b9a4894bcfa9b79
dejanV91 <dejanvukoicic91@gmail.com>
2022-10-17 21:13:42 +0200

    initial commit

--- main.js
+++ main.js
@@ -1,1 +1,4 @@
-console.log("JS ucitan");
+const logo = document.querySelector(".lnXdpd");
+
+logo.src = chrome.runtime.getURL("images/icon128.png");
+logo.srcset = chrome.runtime.getURL("images/icon128.png");
