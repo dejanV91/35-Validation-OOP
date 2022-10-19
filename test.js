8c5a14a01e4ea25b52a940bc120910e0997d6594
dejanV91 <dejanvukoicic91@gmail.com>
2022-10-19 21:04:07 +0200

    initial commit

--- test.js
+++ test.js
@@ -4,11 +4,26 @@ class Validator{
         this.errors = { }
 
         this.generateErrorsObject();
+        this.inputListener();
     }
 
     generateErrorsObject() {
         for(let field in this.elementsConfig) {
             this.errors[field] = [];    
         }
+    }
+
+    inputListener() {
+        let inputSelector = this.elementsConfig;
+
+        for(let field in inputSelector) {
+            let el = document.querySelector(`input[name="${field}"]`);
+
+            el.addEventListener("input", this.validate);
+        }
+    }
+
+    validate(e) {
+        console.log();
     }
 }
